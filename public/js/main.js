941db4924c1390084db93ce31d789fd8729c19b8
NAISHA0608 <naishaayovi00@gmail.com>
2024-05-26 22:03:49 -0500

    RETOJS

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ async function getTrendingMoviesPreview() {
       movieContainer.classList.add('movie-container', 'bg-white', 'p-4', 'rounded', 'shadow');
   
       const movieImg = document.createElement('img');
-      movieImg.classList.add('w-1/2', 'h-48');
+      movieImg.classList.add('w-1/3', 'h-32');
       movieImg.setAttribute('alt', movie.title);
       movieImg.setAttribute(
         'src',
@@ -20,17 +20,34 @@ async function getTrendingMoviesPreview() {
       const information= document.createElement('div');
       information.classList.add('mt-4');
 
+      const fecha= document.createElement('h1');
+      fecha.classList.add('text-lg', 'font-bold', 'text-center');
+      fecha.innerText=movie.release_date;
+
       const titulo= document.createElement('h3');
       titulo.classList.add('text-lg', 'font-bold', 'text-center');
       titulo.innerText=movie.title;
 
+      const lengu= document.createElement('h1');
+      lengu.classList.add('text-lg', 'font-bold', 'text-center');
+      lengu.innerText=movie.original_language;
+      
+
       const description= document.createElement('p');
       description.classList.add('text-gray-600', 'mt-2');
       description.innerText=movie.overview;
+
+      const id= document.createElement('h1');
+      id.classList.add('text-lg', 'font-bold', 'text-center');
+      id.innerText=movie.id;
+
   
       movieContainer.appendChild(movieImg);
       movieContainer.appendChild(information);
       information.appendChild(titulo);
+      information.appendChild(id);
+      information.appendChild(fecha);
+      information.appendChild(lengu);
       information.appendChild(description);
       trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
